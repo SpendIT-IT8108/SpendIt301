5a84cce861c1cea5a5a2683676b5c102a83c2849
FatemaMarhoon <118673837+FatemaMarhoon@users.noreply.github.com>
2022-12-15 08:45:21 +0300

    Initial Commit

---
+++ SpendIt301/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpendIt301
+//
+//  Created by Fatema Marhoon on 15/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
