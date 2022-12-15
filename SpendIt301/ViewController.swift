7a534d5fee360062257563ced82819301c58d9e3
Zainab <zoozii.sweet@gmail.com>
2022-12-15 08:53:49 +0300

    hello world

--- SpendIt301/ViewController.swift
+++ SpendIt301/ViewController.swift
@@ -12,6 +12,16 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        print("hello world")
     }
 
 
