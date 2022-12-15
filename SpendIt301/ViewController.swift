e4df2d6b906681dcf4c5e948f438aa61836dbb3d
MaryamTaraif <maryamtraif02@gmail.com>
2022-12-15 08:58:35 +0300

    hi

--- SpendIt301/ViewController.swift
+++ SpendIt301/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Hello world, Maryam Taraif");
+        print("uffff");
         // Do any additional setup after loading the view.
     }
     
