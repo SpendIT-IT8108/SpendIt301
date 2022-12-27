51a71790f169227b51c5c12782c867a18e8627c4
FatemaMarhoon <118673837+FatemaMarhoon@users.noreply.github.com>
2022-12-27 22:51:58 +0300

    overview

--- OverviewCellsFiles/MostSpentTableViewCell.swift
+++ OverviewCellsFiles/MostSpentTableViewCell.swift
@@ -7,11 +7,47 @@
 
 import UIKit
 
-class MostSpentTableViewCell: UITableViewCell {
+class MostSpentTableViewCell: UITableViewCell,UICollectionViewDataSource,UICollectionViewDelegate  {
 
+
+    @IBOutlet weak var mostSpentCollection: UICollectionView!
+    var mostSpentArray = [mostSpentCategories]()
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        // Initialization code
+        mostSpentCollection.delegate = self
+        mostSpentCollection.dataSource = self
+        
+
+        //define item size
+        let itemSize =
+           NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.3),
+           heightDimension: .fractionalHeight(1))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+//define group size
+        let groupSize =
+           NSCollectionLayoutSize(widthDimension: .fractionalWidth(1),
+           heightDimension: .fractionalWidth(0.5))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize:
+           groupSize, subitem: item, count: 3)
+//define section size
+        let section = NSCollectionLayoutSection(group: group)
+        section.contentInsets = NSDirectionalEdgeInsets(top: 8, leading: 8,
+           bottom: 8, trailing: 8)
+        section.interGroupSpacing = 8
+
+        mostSpentCollection.collectionViewLayout =
+           UICollectionViewCompositionalLayout(section: section)
+        
+
+        mostSpentArray.append(mostSpentCategories(emoji: "💻", categoryName: "Laptop", price: "212 BHD"))
+        mostSpentArray.append(mostSpentCategories(emoji: "🍔", categoryName: "Food", price: "100 BHD"))
+        mostSpentArray.append(mostSpentCategories(emoji: "📚", categoryName: "Books", price: " 50 BHD"))
+        mostSpentArray.append(mostSpentCategories(emoji: "🐱", categoryName: "Cat", price: "50 BHD"))
+       
+        
+       
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -19,5 +55,29 @@ class MostSpentTableViewCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
-
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        if collectionView == self.mostSpentCollection {
+            //register the cell
+        let cell = mostSpentCollection.dequeueReusableCell(withReuseIdentifier: "MostSpentCollectionViewCell", for: indexPath) as! MostSpentCollectionViewCell
+        let category = mostSpentArray[indexPath.row]
+        cell.setUpCell(emoji: category.emoji, categoryName: category.categoryName, price: category.price)
+        
+            return cell }
+        else
+        
+        {
+            return UICollectionViewCell()
+        }
+    }
+    
+    struct mostSpentCategories {
+        let emoji : String
+        let categoryName: String
+        let price : String
+    }
 }
