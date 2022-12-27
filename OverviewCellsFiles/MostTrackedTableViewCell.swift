eeb18c1faa11f1d729b4efef907570ddbcfe3b86
FatemaMarhoon <118673837+FatemaMarhoon@users.noreply.github.com>
2022-12-27 21:57:19 +0300

    chart

---
+++ OverviewCellsFiles/MostTrackedTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MostTrackedTableViewCell.swift
+//  SpendIt301
+//
+//  Created by Fatema Marhoon on 27/12/2022.
+//
+
+import UIKit
+
+class MostTrackedTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
