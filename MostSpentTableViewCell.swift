21758c36dc7741e10709b42acfa0b170e2640299
FatemaMarhoon <118673837+FatemaMarhoon@users.noreply.github.com>
2022-12-28 08:31:08 +0300

    bar chart cell......

---
+++ MostSpentTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MostSpentTableViewCell.swift
+//  SpendIt301
+//
+//  Created by Fatema Marhoon on 28/12/2022.
+//
+
+import UIKit
+
+class MostSpentTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
