924f0074f4032bbb386bb516069c418c2850f58f
Zainab <zoozii.sweet@gmail.com>
2022-12-29 01:03:10 +0300

    category to filter page

---
+++ CatogeryForTransactionCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CatogeryForTransactionCollectionViewCell.swift
+//  SpendIt301
+//
+//  Created by Zainab Muneer on 29/12/2022.
+//
+
+import UIKit
+
+class CatogeryForTransactionCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ CatogeryForTransactionTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CatogeryForTransactionTableViewCell.swift
+//  SpendIt301
+//
+//  Created by Zainab Muneer on 29/12/2022.
+//
+
+import UIKit
+
+class CatogeryForTransactionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
