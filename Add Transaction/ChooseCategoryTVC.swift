17eef0497b10a0193bf5a2de417fa06967fa30f1
KawtharAlakri <202003447@student.polytechnic.bh>
2023-01-06 19:12:10 +0300

    choose category updated

--- Add Transaction/ChooseCategoryTVC.swift
+++ Add Transaction/ChooseCategoryTVC.swift
@@ -10,7 +10,7 @@ import UIKit
 class ChooseCategoryTVC: UITableViewController {
 
     //categories to be displayed in the list
-    var categories : [Category] = Category.loadCategories()!
+    var categories : [Category] = []
     
     //SELECTED CATEGORY
     var type : String
@@ -27,7 +27,7 @@ class ChooseCategoryTVC: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        for cat in categories {
+        for cat in Category.loadCategories()! {
             if cat.type == type {
                 categories.append(cat)
             }
