4ea7d56c43cc7acfcb381940d2ac716081a7f4de
KawtharAlakri <113460164+KawtharAlakri@users.noreply.github.com>
2023-01-09 17:10:54 +0300

    Revert "Setup and walkthrough"

--- SpendIt301/AppDelegate.swift
+++ SpendIt301/AppDelegate.swift
@@ -15,9 +15,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         UNUserNotificationCenter.current().delegate=self
-        
-      
-        
         return true
     }
 
